152094029bb66d8382da2126d6b9dc2f2745a8c0
Javed Mohamed <jav@saronic.com>
2023-08-28 15:29:29 -0500

    Derive home from $HOME instead of /etc/passwd

--- paths.js
+++ paths.js
@@ -3,7 +3,7 @@ const os = require('os');
 module.exports = (process.env['OS'] != 'Windows_NT') ? {
     // Use getent() system call, since this is what ssh does; makes a difference in Docker-based
     // Action runs, where $HOME is different from the pwent
-    homePath: os.userInfo().homedir,
+    homePath: os.homedir(),
     sshAgentCmdDefault: 'ssh-agent',
     sshAddCmdDefault: 'ssh-add',
     gitCmdDefault: 'git'
